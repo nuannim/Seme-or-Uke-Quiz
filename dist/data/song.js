c9b7d6ac8e96d82ea80fe467fdd04e32c61b20c7
nuannim <66070286@kmitl.ac.th>
2026-06-19 18:13:28 +0700

    edit .gitignore

--- dist/data/song.js
+++
@@ -1,9 +0,0 @@
-[
-  {
-    "id": 1,
-    "url": "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/g3RrDbY7FEk?si=GO8JV2oORGf0F2gg\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>",
-    "title": "Ponytail to Shushu",
-    "artist": "BNK48",
-    "artist-cover-url": "images/BNK48_PonytailToShushu.webp"
-  }
-]
